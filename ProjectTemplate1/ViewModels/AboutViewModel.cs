8df6ef8747e6b3eb66e6df5d2ebc76b6834e6b58
Corentin Altepe <corentin.altepe@gmail.com>
2017-04-20 00:59:58 +0300

    Created VSIX project

---
+++ ProjectTemplate1/ViewModels/AboutViewModel.cs
@@ -0,0 +1,39 @@
+﻿using MvvmDialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$.ViewModels
+{
+    class AboutViewModel : ViewModelBase, IModalDialogViewModel
+    {
+        public bool? DialogResult { get { return false; } }
+
+        public string Content
+        {
+            get
+            {
+                return "My Project" + Environment.NewLine +
+                        "Created by Author" + Environment.NewLine +
+                        "Address" + Environment.NewLine +
+                        "Year";
+            }
+        }
+
+        public string VersionText
+        {
+            get
+            {
+                var version1 = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+
+                // For external assemblies
+                // var ver2 = typeof(Assembly1.ClassOfAssembly1).Assembly.GetName().Version;
+                // var ver3 = typeof(Assembly2.ClassOfAssembly2).Assembly.GetName().Version;
+
+                return "MyProject1 v" + version1.ToString();
+            }
+        }
+    }
+}
