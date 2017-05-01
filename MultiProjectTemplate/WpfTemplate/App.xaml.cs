c94011a0efa30be557851a7ca9439c6f0f98963f
Corentin Altepe <corentin.altepe@gmail.com>
2017-05-01 13:53:46 +0300

    Adding log4net NuGet

--- MultiProjectTemplate/WpfTemplate/App.xaml.cs
+++ MultiProjectTemplate/WpfTemplate/App.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
 using System.Windows;
 using $safeprojectname$.ViewModels;
 using $safeprojectname$.Views;
